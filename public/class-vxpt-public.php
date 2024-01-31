049d0f367e44bec303b4632e398fc6431dbb14f5
kmrafibinrabi <kmrafibinrabi97@gmail.com>
2024-01-31 12:43:54 +0600

    changing cdn to assets

--- public/class-vxpt-public.php
+++ public/class-vxpt-public.php
@@ -20,7 +20,7 @@ class Vxpt_Public
         wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/vxpt-public.css', array(), $this->version, 'all');
 
         if (!wp_style_is('fontawesome', 'enqueued')) {
-            wp_register_style('fontawesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', false, '5.15.4');
+            wp_register_style('fontawesome', plugin_dir_url(__FILE__) . '/assets/font-awesome/css/all.min.css', array(), '5.15.4', 'all');
 
             wp_enqueue_style('fontawesome');
         }
